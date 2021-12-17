8e2752a667cd30cd150251e61a7a710cb5c559e8
timo282 <ti-heiss@t-online.de>
2021-12-17 23:28:01 +0100

    improves train-selection method by using dijkstra shortest-path calculation

--- InformatiCupPy/com/informaticup/python/algorithms/SimpleAlgorithmSolver.py
+++ InformatiCupPy/com/informaticup/python/algorithms/SimpleAlgorithmSolver.py
@@ -1,7 +1,7 @@
 from InformatiCupPy.com.informaticup.python.algorithms.ISolver import ISolver
 from InformatiCupPy.com.informaticup.python.objects.Passenger import Passenger
 from InformatiCupPy.com.informaticup.python.algorithms.Graph import Graph
-from InformatiCupPy.com.informaticup.python.algorithms.EasyDijkstraAlgorithm import EasyDijkstraAlgorithm
+from InformatiCupPy.com.informaticup.python.algorithms.EasyDijkstraAlgorithm import EasyDijkstraAlgorithm as Dij
 import sys
 
 
@@ -18,7 +18,8 @@ class SimpleAlgorithmSolver(ISolver):
 
     def solve(self, input):
         """ Method to solve an input problem.
-            input: input list (result of Input_Parser.parse_input())"""
+            :param input: input list (result of Input_Parser.parse_input())
+        """
         # setting up the Graph
         graph = Graph()
         for station in self.stations:
@@ -28,38 +29,69 @@ class SimpleAlgorithmSolver(ISolver):
 
         # starting solving algorithm
         while self.check_break_condition():
-            if self.check_free_train():
+            if self.get_free_trains():
                 focused_passenger = self.choose_next_passenger()
-                chosen_train = self.check_free_train(passenger_group_size=focused_passenger.group_size)
-                _length, _stations, _lanes = EasyDijkstraAlgorithm.dijkstra(graph, chosen_train.position)
+                chosen_train = self.get_nearest_possible_train(passenger=focused_passenger, graph=graph)
+                if focused_passenger.position != chosen_train.position:
+                    _length, _stations, _lanes = \
+                        Dij.calculate_shortest_path(graph, focused_passenger.position, chosen_train.position)
 
                 # for demo reasons:
                 break
 
     def check_break_condition(self):
         """ Checks if the main loop of the algorithm can finish/stop.
-            Returns True as long as there are still passengers who haven't reached their target station yet.
-            Return False if all passengers have already reached their target station. """
+            :return True, as long as there are still passengers who haven't reached their target station yet.
+            :return False, if all passengers have already reached their target station.
+        """
         for passenger in self.passengers:
             if passenger.target_station != passenger.position:
                 return True
         return False
 
-    def check_free_train(self, passenger_group_size=0):
-        """ Checks if there is a free train (train that has enough capacity left for a specific group of passengers).
-            Returns nothing if there is no train that fulfills this condition.
-            Returns, if there is a train to fulfill the condition, this very train.
-            (currently: first train that fulfills condition; better: nearest train)
-            passenger_group_size: size of the passenger group that should fit into the train."""
+    def get_free_trains(self, passenger=None):
+        """ Checks if there are free trains (trains that have enough capacity left for a specific group of passengers)
+            and returns them.
+            :param passenger: optional parameter, passenger that should fit into the train.
+            :return: a list of all trains that fulfill the condition (empty list if no train fulfills condition).
+        """
+        possible_trains = []
+        group_size = 0 if passenger is None else passenger.group_size
         for train in self.trains:
-            if int(train.capacity) - int(passenger_group_size) >= 0:
-                return train  # train should be chosen better - maybe by using dijkstra?
+            if int(train.capacity) - int(group_size) >= 0:
+                possible_trains.append(train)
+        return possible_trains
+
+    def get_nearest_possible_train(self, passenger=None, graph=None):
+        """ Gets the nearest train that can take the passenger (by means of group size).
+            Uses dijkstra shortest-path-calculation to identify the nearest train.
+            :param passenger: passenger that shall use the train (needed for size check and path calculation).
+            :param graph: Graph object of the input map.
+            :return: nearest train.
+        """
+        possible_trains = self.get_free_trains(passenger=passenger)
+        try:
+            best_train = (possible_trains[0], sys.maxsize)  # tuple: (train, distance of train to passenger)
+        except IndexError:
+            return
+
+        for p_train in possible_trains:
+            if p_train.position == passenger.position:  # if there is a train that has already the same position
+                # as the passenger, return that train
+                return p_train
+            # else calculate shortest path:
+            distance_to_passenger, _, __ = Dij.calculate_shortest_path(graph, p_train.position, passenger.position)
+            if distance_to_passenger < best_train[1]:
+                best_train = (p_train, distance_to_passenger)
+
+        return best_train
 
     def choose_next_passenger(self):
         """ Chooses the next passenger with should be brought to its target station.
             Chooses the passenger whose target_time is the lowest (only passengers that haven't reached their
             target station yet can be chosen).
-            Returns the chosen passenger. """
+            Returns the chosen passenger.
+        """
         next_passenger = Passenger(sys.maxsize, sys.maxsize, sys.maxsize, sys.maxsize, sys.maxsize)
         for passenger in self.passengers:
             if int(passenger.target_time) < int(next_passenger.target_time) \
