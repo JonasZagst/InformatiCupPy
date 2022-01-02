14cc646cf6fdb3ae4700284a527a684cccde403e
timo282 <ti-heiss@t-online.de>
2022-01-02 16:33:57 +0100

    Bug fixes and renaming of SimpleAlgorithmSolver to SimpleTrainParallelizationAlgorithm.py

--- InformatiCupPy/com/informaticup/python/Main.py
+++ InformatiCupPy/com/informaticup/python/Main.py
@@ -1,5 +1,6 @@
 from InformatiCupPy.com.informaticup.python.algorithms.EasyDijkstraAlgorithm import EasyDijkstraAlgorithm
-from InformatiCupPy.com.informaticup.python.algorithms.SimpleAlgorithmSolver import SimpleAlgorithmSolver
+from InformatiCupPy.com.informaticup.python.algorithms.SimpleTrainParallelizationAlgorithm \
+    import SimpleTrainParallelizationAlgorithm
 from InformatiCupPy.com.informaticup.python.ioParsing.InputParser import InputParser
 from InformatiCupPy.com.informaticup.python.ioParsing.OutputParser import OutputParser
 import copy
@@ -20,7 +21,7 @@ def main():
 
     # creates a list (length 4) of lists (length x), which contains several object parsed from the input file
     input = InputParser("../input-output/input.txt").parse_input()
-    solvers = [SimpleAlgorithmSolver(copy.deepcopy(input)), EasyDijkstraAlgorithm(copy.deepcopy(input))]
+    solvers = [SimpleTrainParallelizationAlgorithm(copy.deepcopy(input)), EasyDijkstraAlgorithm(copy.deepcopy(input))]
     OutputParser.parse_output_files(solvers, input)
 
 
