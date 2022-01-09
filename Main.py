8ea598402e8771f503b0e521dd4af30312917afd
timo282 <ti-heiss@t-online.de>
2022-01-09 22:09:31 +0100

    Fixes several minor bugs...

--- Main.py
+++ Main.py
@@ -28,8 +28,8 @@ def main():
     # input = InputParser(sys.stdin.read()).parse_input()
     solvers = [EasyDijkstraAlgorithm(copy.deepcopy(input)),
                AdvancedDijkstraAlgorithm(copy.deepcopy(input)),
-               SimpleTrainParallelizationAlgorithm(copy.deepcopy(input), set_wildcards=0.0),
-               SimpleTrainParallelizationAlgorithm(copy.deepcopy(input), parallelization_factor=0.2),
+               #SimpleTrainParallelizationAlgorithm(copy.deepcopy(input), set_wildcards=0.0),
+               #SimpleTrainParallelizationAlgorithm(copy.deepcopy(input), parallelization_factor=0.2),
                SimpleTrainParallelizationAlgorithm(copy.deepcopy(input))
                ]
     OutputParser.parse_output_files(solvers, input)
