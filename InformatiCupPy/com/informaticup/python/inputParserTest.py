2e1e48e1f3312779b849fb8bf3b5e17765b6ddce
mo374z <schlager.mo@t-online.de>
2022-01-16 02:24:25 +0100

    changes in InputGenerator.py

--- InformatiCupPy/com/informaticup/python/inputParserTest.py
+++
@@ -1,3 +0,0 @@
-from InformatiCupPy.com.informaticup.python.ioParsing.InputParser import InputParser
-
-print(InputParser("../../../../InformatiCupPy/com/informaticup/input-output/input.txt").parse_input())
