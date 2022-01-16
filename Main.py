7645eddcc10aa96c1cf36bfe7cdfc91443a017bf
mo374z <schlager.mo@t-online.de>
2022-01-16 11:06:28 +0100

    changes to make implementation without docker

--- Main.py
+++ Main.py
@@ -25,12 +25,13 @@ def main():
         4 Passengers
             id, initial_station, target_station, group_size, target_time
     """
-    # input_str = open("InformatiCupPy/com/informaticup/input-output/input.txt", "r").read()
+
 
     # creates a list (length 4) of lists (length x), which contains several object parsed from the input file
     try:
-        # input = InputParser("InformatiCupPy/com/informaticup/input-output/input.txt").parse_input()
-        input = InputParser(sys.stdin.read()).parse_input()
+        input_str = open("InformatiCupPy/com/informaticup/input-output/input.txt", "r").read()
+        input = InputParser(input_str).parse_input()
+        # input = InputParser(sys.stdin.read()).parse_input()
         solvers = [
             SimpleDijkstraAlgorithm(copy.deepcopy(input)),
             SimplePassengerParallelizationAlgorithm(copy.deepcopy(input), capacity_speed_ratio=0),
@@ -41,14 +42,13 @@ def main():
             AdvancedPassengerParallelizationAlgorithm(copy.deepcopy(input), capacity_speed_ratio=0.3),
             AdvancedPassengerParallelizationAlgorithm(copy.deepcopy(input), capacity_speed_ratio=0.7),
             AdvancedPassengerParallelizationAlgorithm(copy.deepcopy(input), capacity_speed_ratio=1),
-
             SimpleTrainParallelizationAlgorithm(copy.deepcopy(input), set_wildcards=0.0, parallelization_factor=0.0),
             SimpleTrainParallelizationAlgorithm(copy.deepcopy(input), set_wildcards=0.0),
             SimpleTrainParallelizationAlgorithm(copy.deepcopy(input), parallelization_factor=0.2),
             SimpleTrainParallelizationAlgorithm(copy.deepcopy(input))
         ]
-        # OutputParser.parse_output_files(solvers, input)
-        OutputParser.parse_output_files_to_stdout(solvers, input)
+        OutputParser.parse_output_files(solvers, input)
+        # OutputParser.parse_output_files_to_stdout(solvers, input)
     except CannotParseInputException as ex:
         ex.print_message()
 
